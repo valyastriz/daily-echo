aa8f6e9c022ad6af90425a66635a431caf1cc862
CodyBarnes9624 <167149007+CodyBarnes9624@users.noreply.github.com>
2024-08-08 19:02:37 -0400

    added signUp.js file to api folder, added skeletons for the api's for the homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,3 +3,17 @@
 const router = require('express').Router();
 const { Entry, User } = require('../models');
 
+//Have to think about where to put this! 
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home Page' });
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Page' });
+  });
+  
+  app.get('/signup', (req, res) => {
+    res.render('signup', { title: 'Sign Up' });
+  });
+  //Through here
+
