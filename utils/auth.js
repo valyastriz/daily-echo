8b43d211a4170e3d30e229e69466e6ce7ecfcc89
valyastriz <valyastriz@gmail.com>
2024-08-07 20:58:37 -0400

    added auth.js and helpers.js logic as well as some images for logos and renamed login.handlebars to signUp.handlebars

--- utils/auth.js
+++ utils/auth.js
@@ -1,2 +1,15 @@
 // helpers functions and utilities
 // this file will hold Middleware for authentication
+const withAuth = (req, res, next) => {
+    // TODO: If the user is not logged in, redirect the user to the login page
+    // TODO: If the user is logged in, allow them to view the paintings
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+  
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
