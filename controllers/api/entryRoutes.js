675a3f7b082081e0bf5a307d30b97ea7914681fb
valyastriz <valyastriz@gmail.com>
2024-08-08 18:20:23 -0400

    added router.get by id to entryRoutes.js file. will need to uncomment the wit auth part later

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const { Entry } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
 // get all diary entries
+// router.get('/', withAuth, async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const entries = await Entry.findAll();
@@ -15,8 +17,24 @@ router.get('/', async (req, res) => {
 });
 
 // get single diary entry
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const singleEntry = await Entry.findOne({
+            where: {
+                id: req.params.id,
+                // user_id: req.session ? req.session.user_id : null,
+            }
+        });
 
+        if (!singleEntry) {
+            res.status(404).json({ message: 'No entry found with this id for the current user' });
+            return;
+        }
+
+        res.status(200).json(singleEntry);
+    }catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // create a new entry
