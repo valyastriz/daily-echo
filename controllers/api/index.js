6501fe0b33786b442c1f44ea05bbbd1c1e6788cb
Abdihakim2334 <abdihakim2334@gmail.com>
2024-08-12 21:17:51 -0400

    added emailroutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const entryRoutes = require('./entryRoutes');
+const emailRoutes = require('./emailRoutes');
 
 router.use('/users', userRoutes);
 router.use('/entries', entryRoutes);
+router.use('/email', emailRoutes);
 
 module.exports = router;
